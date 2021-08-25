4380ab5614f7cfa7c9f2fe628f7ffe2fac847048
Sanusi Cole <sanusi1612@icloud.com>
2021-08-25 15:45:43 -0400

    adds inquir questions

---
+++ index.js
@@ -0,0 +1,61 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+// create writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      name: 'title',   
+      message: 'What is the name of the app?',
+    },
+    {
+        name: 'desc',
+        message: 'enter a description?',
+      },
+      {
+        name: 'install',
+        message: 'what is the install command for your app?',
+      },
+      {
+        name: 'usage',
+        message: 'how do you start the app?',
+      },
+    {
+      name: 'contributing',
+      message: 'Who are the contributers?',
+    },
+    {
+      name: 'test',
+      message: 'What is the command to run test?',
+    },
+    {
+      name: 'license',
+      message: 'What license have you chosen?',
+    type: 'list',options: ["MIT","APACHE 2.0"]
+    },
+    {
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      name: 'email',
+      message: 'Enter your email address.',
+    },
+  ]);
+};
+
+const generatereadme = (answers) =>
+  ``;
+
+// Bonus using writeFileAsync as a promise
+const init = () => {
+  promptUser()
+    .then((answers) => writeFileAsync('./sample/README.md', generatereadme(answers)))
+    .then(() => console.log('Successfully wrote readme to sample folder'))
+    .catch((err) => console.error(err));
+};
+
+init();
